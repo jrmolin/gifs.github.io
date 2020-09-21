b526e9fd18e35ab6a7ed624db3b9a595ff03b4d3
mo <jmolinatto@gmail.com>
2020-09-21 10:50:20 -0400

    add ants.jpg

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("static");
     return {
         dir: {
-            output: "docs",
+            output: "../gh-pages/",
         }
     };
 };
